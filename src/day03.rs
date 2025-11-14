1363d6b578d8d5de69239011452dba74c8855e70
loganbarnhart01 <logan.barnhart.01@gmail.com>
2025-11-13 23:31:56 -0700

    day 3 part 2

--- src/day03.rs
+++ src/day03.rs
@@ -7,6 +7,10 @@ pub fn solve() {
         Ok(contents) => println!("Part 1: {:?}", contents),
         Err(e) => println!("Error in part 1: {}", e),
     }
+    match part_2() {
+        Ok(contents) => println!("Part 2: {:?}", contents),
+        Err(e) => println!("Error in part 1: {}", e),
+    }
 }
 
 fn part_1() -> io::Result<u32> {
@@ -35,6 +39,48 @@ fn part_1() -> io::Result<u32> {
                 let mult1: u32 = cast_chars_to_int(&input[head+4..comma_idx]).unwrap();
                 let mult2: u32 = cast_chars_to_int(&input[comma_idx+1..tail]).unwrap();
                 result += mult1 * mult2;
+            }
+            head = tail;
+        } else {
+            head += 1;
+        }
+    }
+    Ok(result)
+}
+
+fn part_2() -> io::Result<u32> {
+    let input: Vec<char> = load_input_chars().unwrap();
+    if input.len() < 8 { return Ok(0) }
+    let mut result: u32 = 0; 
+    let mut head: usize = 0;
+    let mut tail: usize = 0;
+    let mut do_on: u32 = 1;
+    while head + 7 < input.len() && tail < input.len(){
+        if input[head..head+4] == ['d', 'o', '(', ')'] { // look for valid start first
+            do_on = 1;
+            head = head + 4;
+        } else if input[head..head+7] == ['d', 'o', 'n', '\'', 't', '(', ')'] { // look for valid start first
+            do_on = 0;
+            head = head + 6;
+        } else if input[head..head+4] == ['m', 'u', 'l', '('] { // look for valid start first
+            tail = head + 4;  
+            let mut valid_mul: bool = true;
+            let mut comma_idx: usize = 0;                // keep track of comma for parsing
+            while tail < input.len() {
+                tail += 1;
+                if input[tail] == ')' { 
+                    break;
+                } else if input[tail] != ',' && !input[tail].is_numeric() {
+                    valid_mul = false;
+                    break;
+                } else if input[tail] == ',' {
+                    comma_idx = tail;
+                }
+            }
+            if valid_mul {
+                let mult1: u32 = cast_chars_to_int(&input[head+4..comma_idx]).unwrap();
+                let mult2: u32 = cast_chars_to_int(&input[comma_idx+1..tail]).unwrap();
+                result += do_on * mult1 * mult2;
             }
             head = tail;
         } else {
