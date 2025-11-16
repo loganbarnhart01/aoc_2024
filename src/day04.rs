8ec0fce9be3a271365d1772fcdb3b878911870de
Logan Barnhart <logan.barnhart.01@gmail.com>
2025-11-15 19:57:42 -0700

    day 4 part 2

--- src/day04.rs
+++ src/day04.rs
@@ -6,7 +6,7 @@ pub fn solve(input_file: &String) {
         Ok(contents) => println!("Part 1: {:?}", contents),
         Err(e) => println!("Error in part 1: {}", e),
     }
-    match part_2() {
+    match part_2(input_file) {
         Ok(contents) => println!("Part 2: {:?}", contents),
         Err(e) => println!("Error in part 1: {}", e),
     }
@@ -14,14 +14,95 @@ pub fn solve(input_file: &String) {
 
 fn part_1(input_file: &str) -> io::Result<u32> {
     let input = load_input_chars(input_file).unwrap();
-    println!("{:?}", input[0][3]);
     let mut xmas = 0;
     for row_idx in 0..input.len() {
         for col_idx in 0..input[0].len() {
-            xmas += check_words_at(&input, row_idx, col_idx).unwrap(); 
+            xmas += check_word_at(&input, row_idx, col_idx, Direction::Right, "XMAS").unwrap();
+            xmas += check_word_at(&input, row_idx, col_idx, Direction::Right, "SAMX").unwrap();
+
+            xmas += check_word_at(&input, row_idx, col_idx, Direction::Down, "XMAS").unwrap();
+            xmas += check_word_at(&input, row_idx, col_idx, Direction::Down, "SAMX").unwrap();
+
+            xmas += check_word_at(&input, row_idx, col_idx, Direction::DRight, "XMAS").unwrap();
+            xmas += check_word_at(&input, row_idx, col_idx, Direction::DRight, "SAMX").unwrap();
+
+            xmas += check_word_at(&input, row_idx, col_idx, Direction::URight, "XMAS").unwrap();
+            xmas += check_word_at(&input, row_idx, col_idx, Direction::URight, "SAMX").unwrap();
         }
     }
     Ok(xmas)   
+}
+
+fn part_2(input_file: &str) -> io::Result<u32> {
+    let input = load_input_chars(input_file).unwrap();
+    let mut x = 0;
+    for row_idx in 0..input.len()-2 {
+        for col_idx in 0..input[0].len() {
+            let mut mas = 0;
+            let mut sam = 0;
+            mas += check_word_at(&input, row_idx, col_idx, Direction::DRight, "MAS").unwrap();
+            sam += check_word_at(&input, row_idx, col_idx, Direction::DRight, "SAM").unwrap();
+
+            mas += check_word_at(&input, row_idx + 2, col_idx, Direction::URight, "MAS").unwrap();
+            sam += check_word_at(&input, row_idx + 2, col_idx, Direction::URight, "SAM").unwrap();
+
+            if mas + sam >= 2 { x  += 1 }
+        }
+    }
+    Ok(x)   
+}
+
+
+#[derive(Debug)]
+enum Direction {
+    Right,
+    Down,
+    URight,
+    DRight,
+}
+
+fn check_word_at(mat: &Vec<Vec<char>>, row_idx: usize, col_idx: usize, direction: Direction, word: &str) -> io::Result<u32> {
+    let search: bool;
+    let mut row_mult: i32 = 0;
+    let mut col_mult: usize = 0;
+    match direction {
+        Direction::Right => { 
+            search = mat.len() - col_idx >= word.len(); 
+            col_mult = 1;
+        },
+        Direction::Down => { 
+            search = mat[0].len() - row_idx >= word.len(); 
+            row_mult = 1;
+        },
+        Direction::DRight => { 
+            search = mat[0].len() - row_idx >= word.len() && mat.len() - col_idx >= word.len(); 
+            row_mult = 1;
+            col_mult = 1;
+        },
+        Direction::URight => { 
+            search = row_idx >= word.len()-1 && mat.len() - col_idx >= word.len(); 
+            col_mult = 1;
+            row_mult = -1;
+        },
+    }
+
+    if search {
+        let word_present: bool = &(0..word.len())
+            .map(|ii| mat[usize::try_from(i32::try_from(row_idx).unwrap() + row_mult * i32::try_from(ii).unwrap()).unwrap()][col_idx + col_mult * ii])
+            .collect::<String>() == word;
+        return Ok(u32::from(word_present))
+    }
+    Ok(0)
+}
+
+
+fn load_input_chars(input_file: &str) -> io::Result<Vec<Vec<char>>> {
+    let content: String = fs::read_to_string(input_file)?;
+    let mut lines = Vec::new();
+    for line in content.lines() {
+        lines.push(line.chars().collect());
+    }
+    Ok(lines)
 }
 
 // I learned that I have python brain and am taking indexing values for granted. 
@@ -33,55 +114,3 @@ fn part_1(input_file: &str) -> io::Result<u32> {
 //
 // Instead, mat[row_idx..row_idx+4] returns a slice of rows from row_idx..row_idx + 4, then we select col_idx
 // from those. 
-
-fn check_words_at(mat: &Vec<Vec<char>>, row_idx: usize, col_idx: usize) -> io::Result<u32> {
-    let fwd = ['X', 'M', 'A', 'S'];
-    let bwd = ['S', 'A', 'M', 'X'];
-    let search_row = mat.len() - col_idx >= 4;
-    let search_down = mat[0].len() - row_idx >= 4;
-    let search_up = row_idx >= 3;
-
-    let mut found = 0;
-    if search_row {
-        let horz: Vec<char> = (0..4)
-            .map(|ii| mat[row_idx][col_idx + ii])
-            .collect();
-        if horz == fwd { found += 1 }
-        else if horz == bwd { found += 1 }
-    }
-    if search_down {
-        let vert: Vec<char> = (0..4)
-            .map(|ii| mat[row_idx + ii][col_idx])
-            .collect();
-        if vert == &fwd { found += 1 }
-        else if vert == &bwd { found += 1 }
-    }
-    if search_row && search_down { // look for diag down right 
-        let diag: Vec<char> = (0..4)
-            .map(|offset| mat[row_idx + offset][col_idx + offset])
-            .collect();
-        if diag == fwd { found += 1 }
-        if diag == bwd { found += 1 }
-    }
-    if search_row && search_up {   // look for diag up right 
-        let diag: Vec<char> = (0..4)
-            .map(|offset| mat[row_idx - offset][col_idx + offset])
-            .collect();
-        if diag == fwd { found += 1 }
-        if diag == bwd { found += 1 }
-    }
-    Ok(found)
-}
-
-fn part_2() -> io::Result<u32> {
-    Ok(1)   
-}
-
-fn load_input_chars(input_file: &str) -> io::Result<Vec<Vec<char>>> {
-    let content: String = fs::read_to_string(input_file)?;
-    let mut lines = Vec::new();
-    for line in content.lines() {
-        lines.push(line.chars().collect());
-    }
-    Ok(lines)
-}
